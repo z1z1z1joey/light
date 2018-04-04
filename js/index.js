09930ba985459607560a7b4e6d3db341648acc41
z1z1z1joey <z1z1z1joey@gmail.com>
2018-04-04 13:28:31 +0800

    修正計時器及輸入數字功能

--- js/index.js
+++ js/index.js
@@ -1,25 +1,42 @@
 "use strict";
 
 var state = true;
-var timeLeft = 5;
+var timeLeft =  6 ;
 var id = setInterval(countDownTimer, 1000);
 
-//計時器
-function countDownTimer(){
-   timeLeft -= 1;
-   if (timeLeft <= 0) {
-    document.getElementById("image").src = "img/light-off.png" ; 
-    clearInterval(id);
-   }
-   document.getElementById("timer").innerHTML = timeLeft;
+function createToDo() {
+    var input = document.getElementById("input").value ;
+
+    if (isNaN(input) == true) {
+      alert("請輸入數字");
+    }
+    else
+      timeLeft = Number(input)+ 1  ;
+      id = setInterval(countDownTimer, 1000);
+      
 }
-
-
 //切換照片
 function toggleLight(){
    var image = document.getElementById("image");
    state = ! state ;
-  image.src = state ? "img/light-on.png" : "img/light-off.png";  
+   image.src = state ? "img/light-on.png" : "img/light-off.png";  
+   timeLeft = 6;  
+}
+
+//計時器
+function countDownTimer(){
+   
+   if (timeLeft <= 0) {
+    document.getElementById("image").src = "img/light-off.png" ; 
+    clearInterval(id);
+   }else {
+    timeLeft -= 1;
+    image.src ="img/light-on.png";
+   }
+   document.getElementById("timer").innerHTML = timeLeft;
+
 
 }
+
+
 
