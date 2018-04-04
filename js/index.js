b50f89560ccf35b24b985d29a0014b59d8d27dce
z1z1z1joey <z1z1z1joey@gmail.com>
2018-04-04 23:36:50 +0800

    停止後重新輸入

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 "use strict";
 
 var state = true;
-var timeLeft =  6 ;
-var id = setInterval(countDownTimer, 1000);
+var timeLeft = "" ;
+var id = "";
 
 function createToDo() {
     var input = document.getElementById("input").value ;
@@ -10,9 +10,10 @@ function createToDo() {
     if (isNaN(input) == true) {
       alert("請輸入數字");
     }
-    else
+    else{
       timeLeft = Number(input)+ 1  ;
       id = setInterval(countDownTimer, 1000);
+    }
       
 }
 //切換照片
@@ -20,7 +21,7 @@ function toggleLight(){
    var image = document.getElementById("image");
    state = ! state ;
    image.src = state ? "img/light-on.png" : "img/light-off.png";  
-   timeLeft = 6;  
+    
 }
 
 //計時器
@@ -28,11 +29,14 @@ function countDownTimer(){
    
    if (timeLeft <= 0) {
     document.getElementById("image").src = "img/light-off.png" ; 
-    clearInterval(id);
     alert("時間到了");
+    clearInterval(id);
+    document.getElementById("btn").disabled=false;
+    
    }else {
     timeLeft -= 1;
     image.src ="img/light-on.png";
+    document.getElementById("btn").disabled=true;
    }
    document.getElementById("timer").innerHTML = timeLeft;
 
